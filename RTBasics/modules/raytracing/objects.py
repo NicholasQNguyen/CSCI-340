ae9268f1dcb7ba5d8e3a5ba787eda87d47576dde
NicholasQNguyen <NicholasQNguyen8@gmail.com>
2023-02-03 15:37:46 -0500

    finished refactoring objects and lights

--- RTBasics/modules/raytracing/objects.py
+++ RTBasics/modules/raytracing/objects.py
@@ -2,6 +2,7 @@
 Author: Liz Matthews, Geoff Matthews
 """
 from abc import ABC, abstractmethod
+import numpy as np
 
 class Object3D(ABC):
     """Abstract base class for all objects in the raytraced scene.
@@ -45,3 +46,49 @@ class Object3D(ABC):
     def getNormal(self, intersection):
         """Find the normal for the given object. Must override."""
         pass
+
+
+class Sphere(Object3D):
+    def __init__(self, radius, position, ambient, diffuse,
+                 specular, shininess, specCoeff):
+        super().__init__(position, True)
+        self.material = Material(ambient, diffuse, specular, shininess)
+        self.radius = radius
+
+    def getRadius(self):
+        return self.radius
+        
+    def intersect(self, ray):
+        """Find the intersection for the given object. Must override."""
+        pass
+   
+    def getNormal(self, intersection):
+        """Find the normal for the given object. Must override."""
+        pass
+
+
+class Plane(Object3D):
+    def __init__(self, normal, position, ambient, diffuse, specular,
+                 shininess, specCoeff):
+        super().__init__(position, False)
+        self.material = Material(ambient, diffuse, specular, shininess)
+        self.normal = normal
+
+    def getNormal(self):
+        return self.normal
+
+    def intersect(self, ray):
+        """Find the intersection for the given object. Must override."""
+        pass
+   
+    def getNormal(self, intersection):
+        """Find the normal for the given object. Must override."""
+        pass
+
+
+class Material(object):
+    def __init__(self, ambient, diffuse, specular, shininess):
+        self.ambient = ambient
+        self.diffuse = diffuse
+        self.specular = specular
+        self.shininess = shininess
