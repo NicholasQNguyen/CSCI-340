f286d36ce1385a57a216eadb82483c634bd2fee3
NicholasQNguyen <nicholasqnguyen8@gmail.com>
2023-02-20 19:19:03 -0500

    implemented cubes? not really

--- RTAdvanced/modules/raytracing/planar.py
+++ RTAdvanced/modules/raytracing/planar.py
@@ -1,19 +1,22 @@
+import sys
 import numpy as np
+from enum import Enum
 
 from .objects import Object3D
 from .materials import Material
 
+class Side(Enum):
+    """Each side of the cube."""
+    Top = 0
+    Botton = 1
+    Left = 2
+    Right = 3
+    Front = 4
+    Bottom = 5
+
 
 class Planar(Object3D):
-    def intersect(self, ray):
-        """Find the intersection for the plane."""
-        # 10 Slides, slide 16
-        denom = np.dot(ray.direction, self.normal)
-        if denom == 0:
-            return np.inf
-        q = self.position - ray.position
-        t = (np.dot(q, self.normal)) / denom
-        return self.positiveOnly(t)
+    pass
 
 
 class Plane(Planar):
@@ -26,6 +29,16 @@ class Plane(Planar):
     def getNormal(self, intersection):
         """Find the normal for the given object. Must override."""
         return self.normal
+
+    def intersect(self, ray):
+        """Find the intersection for the plane."""
+        # 10 Slides, slide 16
+        denom = np.dot(ray.direction, self.normal)
+        if denom == 0:
+            return np.inf
+        q = self.position - ray.position
+        t = (np.dot(q, self.normal)) / denom
+        return self.positiveOnly(t)
 
     def __repr__(self):
         return str(self.getBaseColor()) + " Plane"
@@ -40,10 +53,66 @@ class Cube(Planar):
         self.sides = []
         self.setSides()
 
+    def setSides(self): 
+        addSide(Side.Top)
+        addSide(Side.Bottom)
+        addSide(Side.Left)
+        addSide(Side.Right)
+        addSide(Side.Front)
+        addSide(Side.Back)
+
     def intersect(self, ray):
         """Find the intersection for the cube."""
-        t = 1.0
+        ts = [side.intersect(ray) for side in self.sides]
+        filteredTs = [t for t in ts if t is not np.inf]
         return self.positiveOnly(t)
+
+    def addSide(self, side):
+        distance = self.length / 2
+        match side:
+            case Side.Top:
+                normal = vec(0, 1, 0)
+                position = self.position + vec(0,
+                                               distance,
+                                               0)
+            case Side.Bottom:
+                normal = vec(0, -1, 0)
+                position = self.position - vec(0,
+                                               distance,
+                                               0)
+            case Side.Left:
+                normal = vec(-1, 0, 0)
+                position = self.position - vec(distance,
+                                               0,
+                                               0)
+            case Side.Right:
+                normal = vec(1, 0, 0)
+                position = self.position + vec(distance,
+                                               0,
+                                               0)
+            case Side.Front:
+                normal = vec(0, 0, 1)
+                position = self.position + vec(0,
+                                               0,
+                                               distance)
+            case Side.Back:
+                normal = vec(0, 0, -1)
+                position = self.position - vec(0,
+                                               0,
+                                               distance)
+            case _:
+                print("OH GOD")
+                sys.exit(1)
+
+        self.sides.append(normal=normal,
+                          position=position,
+                          color=self.color,
+                          ambient=self.ambient,
+                          diffuse=self.diffuse,
+                          specular=self.specular,
+                          shininess=self.shininess,
+                          specCoeff=self.specularCoefficient)
+        
 
     def __repr__(self):
         return str(self.getBaseColor()) + " Cube"
