14996f884a06417958d6326821ca27232e27ea80
NicholasQNguyen <nicholasqnguyen8@gmail.com>
2023-03-26 14:31:31 -0400

    added make scale asymmetric

---
+++ OGLBasics/modules/geometry/colorFuncs.py
@@ -0,0 +1,11 @@
+"""
+Author: Liz Matthews
+"""
+import numpy as np
+import random
+from ..utils.vector import vec, lerp
+
+
+def randomColor(u, v, uMaxIndex, vMaxIndex):
+    return vec(random.random(), random.random(), random.random())
+
