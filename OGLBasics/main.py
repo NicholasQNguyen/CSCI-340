6aab350c3d6052529a9820fdc7e654eb04fb9b6b
NicholasQNguyen <nicholasqnguyen8@gmail.com>
2023-03-30 23:50:43 -0400

    finished table and got stuff in the bowl

--- OGLBasics/main.py
+++ OGLBasics/main.py
@@ -10,11 +10,13 @@ import numpy as np
 
 from modules.oGL.base import Base
 from modules.renderer import Renderer
-from modules.objects import Scene, Camera, MovingMesh, AxesHelper, GridHelper
+from modules.objects import Scene, Camera, MovingMesh, AxesHelper, GridHelper, Group
 from modules.geometry import (BoxGeometry,
                               SphereGeometry,
                               PyramidGeometry,
-                              RectangleGeometry)
+                              RectangleGeometry,
+                              PolygonGeometry,
+                              CylinderGeometry)
 from modules.geometry.colorFuncs import (randomColor, rainbowGradient,
                                          purple, blue)
 from modules.movementRig import MovementRig
@@ -84,6 +86,75 @@ class Main(Base):
         floorMesh.setRotate(3 * np.pi / 2, 0, 0)
         floorMesh.setRotationalSpeed(0)
         self.scene.add(floorMesh)
+        # Table
+        tableGroup = Group()
+        # Table top
+        if self.materialType == "point":
+            tableTopMaterial = PointMaterial({"useVertexColors": True})
+        elif self.materialType == "line":
+            tableTopMaterial = LineMaterial({"useVertexColors": True})
+        else:
+            tableTopMaterial = SurfaceMaterial({"useVertexColors": True})
+        tableTopGeometry = BoxGeometry(width=3, height=3, depth=0.5)
+        tableTopMesh = MovingMesh(tableTopGeometry, floorMaterial)
+        tableGroup.add(tableTopMesh)
+        tableTopMesh.setRotate(3 * np.pi / 2, 0, 0)
+        tableTopMesh.setRotationalSpeed(0)
+        tableTopMesh.setPosition([-4, 1, -4])
+        # Table legs
+        if self.materialType == "point":
+            tableLeg1Material = PointMaterial({"useVertexColors": True})
+        elif self.materialType == "line":
+            tableLeg1Material = LineMaterial({"useVertexColors": True})
+        else:
+            tableLeg1Material = SurfaceMaterial({"useVertexColors": True})
+        tableLeg1Geometry = BoxGeometry(width=0.5, height=0.5, depth=1)
+        tableLeg1Mesh = MovingMesh(tableLeg1Geometry, floorMaterial)
+        tableGroup.add(tableLeg1Mesh)
+        tableLeg1Mesh.setRotate(3 * np.pi / 2, 0, 0)
+        tableLeg1Mesh.setRotationalSpeed(0)
+        tableLeg1Mesh.setPosition([-5, 0.5, -5])
+
+        if self.materialType == "point":
+            tableLeg2Material = PointMaterial({"useVertexColors": True})
+        elif self.materialType == "line":
+            tableLeg2Material = LineMaterial({"useVertexColors": True})
+        else:
+            tableLeg2Material = SurfaceMaterial({"useVertexColors": True})
+        tableLeg2Geometry = BoxGeometry(width=0.5, height=0.5, depth=1)
+        tableLeg2Mesh = MovingMesh(tableLeg2Geometry, floorMaterial)
+        tableGroup.add(tableLeg2Mesh)
+        tableLeg2Mesh.setRotate(3 * np.pi / 2, 0, 0)
+        tableLeg2Mesh.setRotationalSpeed(0)
+        tableLeg2Mesh.setPosition([-3, 0.5, -3])
+
+        if self.materialType == "point":
+            tableLeg3Material = PointMaterial({"useVertexColors": True})
+        elif self.materialType == "line":
+            tableLeg3Material = LineMaterial({"useVertexColors": True})
+        else:
+            tableLeg3Material = SurfaceMaterial({"useVertexColors": True})
+        tableLeg3Geometry = BoxGeometry(width=0.5, height=0.5, depth=1)
+        tableLeg3Mesh = MovingMesh(tableLeg3Geometry, floorMaterial)
+        tableGroup.add(tableLeg3Mesh)
+        tableLeg3Mesh.setRotate(3 * np.pi / 2, 0, 0)
+        tableLeg3Mesh.setRotationalSpeed(0)
+        tableLeg3Mesh.setPosition([-3, 0.5, -5])
+
+        if self.materialType == "point":
+            tableLeg4Material = PointMaterial({"useVertexColors": True})
+        elif self.materialType == "line":
+            tableLeg4Material = LineMaterial({"useVertexColors": True})
+        else:
+            tableLeg4Material = SurfaceMaterial({"useVertexColors": True})
+        tableLeg4Geometry = BoxGeometry(width=0.5, height=0.5, depth=1)
+        tableLeg4Mesh = MovingMesh(tableLeg4Geometry, floorMaterial)
+        tableGroup.add(tableLeg4Mesh)
+        tableLeg4Mesh.setRotate(3 * np.pi / 2, 0, 0)
+        tableLeg4Mesh.setRotationalSpeed(0)
+        tableLeg4Mesh.setPosition([-5, 0.5, -3])
+        self.scene.add(tableGroup)
+    
         # Bowl
         if self.materialType == "point":
             sphereMaterial = PointMaterial({"useVertexColors": True},
@@ -99,9 +170,38 @@ class Main(Base):
                                         vStart=-np.pi/2, vEnd=np.pi/2,
                                         colorFunction=rainbowGradient)
         sphereMesh = MovingMesh(sphereGeometry, sphereMaterial)
-        sphereMesh.setPosition([4, 1, -4])
+        sphereMesh.setPosition([-4, 1.75, -4])
         sphereMesh.setRotate(0, 0, 3*np.pi/2)
         self.scene.add(sphereMesh)
+        # Stuff in the bowl
+        if self.materialType == "point":
+            polygonMaterial = PointMaterial({"useVertexColors": True},
+                                           doubleSide=True)
+        elif self.materialType == "line":
+            polygonMaterial = LineMaterial({"useVertexColors": True},
+                                           doubleSide=True)
+        else:
+            polygonMaterial = SurfaceMaterial({"useVertexColors": True},
+                                           doubleSide=True)
+        polygonGeometry = PolygonGeometry(radius=.25, sides=8)
+        polygonMesh = MovingMesh(polygonGeometry, polygonMaterial)
+        polygonMesh.setPosition([-4.25, 1.75, -4])
+        polygonMesh.setRotate(0, 0, 3*np.pi/2)
+        self.scene.add(polygonMesh)
+    
+        if self.materialType == "point":
+            cylinderMaterial = PointMaterial({"useVertexColors": True},
+                                           doubleSide=True)
+        elif self.materialType == "line":
+            cylinderMaterial = LineMaterial({"useVertexColors": True},
+                                           doubleSide=True)
+        else:
+            cylinderMaterial = SurfaceMaterial({"useVertexColors": True},
+                                           doubleSide=True)
+        cylinderGeometry = CylinderGeometry(radius=0.125, height=0.5)
+        cylinderMesh = MovingMesh(cylinderGeometry, cylinderMaterial)
+        cylinderMesh.setPosition([-3.75, 1.75, -4])
+        self.scene.add(cylinderMesh)
 
     def update(self):
         """Most of the work is in scene, rig, and renderer!"""
