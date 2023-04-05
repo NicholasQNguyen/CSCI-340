a6d85a42ad82c082158c96fef8c4a79a09fec7b6
NicholasQNguyen <nicholasqnguyen8@gmail.com>
2023-04-04 20:45:48 -0400

    moved some image code into the shader code file

--- OGLAdvanced/modules/materials/imageMaterial.py
+++ OGLAdvanced/modules/materials/imageMaterial.py
@@ -1,6 +1,6 @@
 from .abstract import AbstractMaterial
 from .texture import Texture
-from ..oGL.shaderCode import lightCalcPhong, lightStruct, vertexShaderWithNormals
+from ..oGL.shaderCode import lightCalcPhong, lightStruct, vertexShaderWithNormals, imageVertexShader, imageFragmentShader
 from OpenGL.GL import *
 
 class ImageMaterial(AbstractMaterial):
@@ -9,83 +9,11 @@ class ImageMaterial(AbstractMaterial):
         self.texture = Texture(texture)
         
         vertexShaderCode = \
-        """
-        uniform mat4 projectionMatrix;
-        uniform mat4 viewMatrix;
-        uniform mat4 modelMatrix;
-        uniform vec3 baseColor;
-        uniform vec2 repeatUV;
-        uniform vec2 offsetUV;
-        uniform bool useVertexColors;
-        uniform bool useFaceNormals;
-        in vec3 vertexColor;
-        in vec3 vertexPosition;
-        in vec3 vertexNormal;
-        in vec3 faceNormal;
-        in vec2 vertexUV;
-        out vec3 color;
-        out vec3 normal;        
-        out vec3 position;
-        out vec2 UV;
-        void main() {
-            gl_Position = projectionMatrix * viewMatrix *
-                          modelMatrix * vec4(vertexPosition, 1);
-                          
-            position = vec3(modelMatrix * vec4(vertexPosition, 1));
-            
-            if (useFaceNormals)
-                normal = normalize(mat3(modelMatrix) * faceNormal);
-            else
-                normal = normalize(mat3(modelMatrix) * vertexNormal);
-            
-            color = baseColor;
-            if (useVertexColors)
-                {
-                color *= vertexColor;
-                }
-            UV = vertexUV * repeatUV + offsetUV;
-        }
-        """
+        imageVertexShader
 
         fragmentShaderCode = \
-        lightStruct + lightCalcPhong + \
-        """
-        uniform sampler2D texture;
-        uniform bool useVertexColors;
-        uniform float ambMul;
-        uniform float specMul;
-        uniform bool useUVColors;
-        in vec3 color;
-        in vec3 normal;
-        in vec3 position;
-        in vec2 UV;
-        out vec4 fragColor;
-        void main()
-        {
-            vec3 diffuse = color;
-            if (useUVColors)
-            {
-                vec4 tColor = texture2D(texture, UV);
-                if (tColor.z < 0.01)
-                {
-                    discard;
-                }
-                diffuse *= tColor.rgb;
-            }
-            vec3 total =  diffuse;
-            total = total * ambMul;
-            vec3 specular = (diffuse + vec3(0.1,0.1,0.1)) * specMul;
-            total = lightCalc(light0, total, diffuse, specular,
-                              position, normal);
-            total = lightCalc(light1, total, diffuse, specular,
-                              position, normal);
-            total = lightCalc(light2, total, diffuse, specular,
-                              position, normal);
-            total = lightCalc(light3, total, diffuse, specular,
-                              position, normal);
-            fragColor = vec4(total, 1);
-        }
-        """
+        lightStruct + lightCalcPhong + imageFragmentShader
+
         super().__init__(vertexShaderCode, fragmentShaderCode)
         self.addUniform("Light", "light0", None )
         self.addUniform("Light", "light1", None )
